0e0eabaffd24b8a4781b808f902432fe2b0c6b5c
Donlimsgp <don_lim@tech.gov.sg>
2024-10-29 14:00:59 +0000

    Implement Google Map

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// Initialize and add the map
-function initMap() {
-    // The location to be shown on the map (e.g., Health Promotion Board)
-    const location = { lat: 1.2801, lng: 103.8441 };
+// script.js
 
-    // The map, centered at the location
+function initMap() {
+    const location = { lat: 1.2801, lng: 103.8441 }; // Example coordinates
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 16,
         center: location,
     });
-
-    // The marker, positioned at the location
-    const marker = new google.maps.Marker({
+    new google.maps.Marker({
         position: location,
         map: map,
     });
